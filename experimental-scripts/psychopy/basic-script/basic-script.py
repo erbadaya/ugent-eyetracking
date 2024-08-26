94cc03d6650b36eb226eef9e787958f6ac3dafc6
erbadaya <erbadaya@gmail.com>
2024-08-26 14:37:56 +0200

    tidy and comment more basic script

--- experimental-scripts/psychopy/basic-script/basic-script.py
+++ experimental-scripts/psychopy/basic-script/basic-script.py
@@ -1,4 +1,7 @@
 # Basic script eye-tracking
+# Author: Esperanza Badaya
+# 21/11/2023
+
 # NB we have created a very simple experiment to illustrate the steps described in Chapter 11
 # In this experiment, we are *not* covering regions of interest (see Visual World Paradigm example for this)
 # We are sending minimal (and redundant triggers) to illustrate how it is done, but NB that these triggers are redundant
@@ -30,6 +33,8 @@
 # without the need of the eye-tracker
 # we will do that via the variable dummy_mode
 
+# Last update: 26/08/2024
+
 # Libraries
 
 from psychopy import gui, visual, event, logging
@@ -38,6 +43,20 @@ import time, os, numpy
 # eye-tracking libraries
 import pylink
 from EyeLinkCoreGraphicsPsychoPy import EyeLinkCoreGraphicsPsychoPy # remember to put this script in your folder
+
+# Set up a a variable to run the script on a computer not connected to the tracker
+# We will use this variable in a series of if-else statements everytime there would be a line of code calling the tracker
+# We will change it to 'False' when running the experiment in the lab
+
+dummy_mode = True
+
+# Create screen
+# You could then call win.size[0] and win.size[1] to get the width and height of the screen respectively and avoid typing it as is done in this example
+
+win = visual.Window(fullscr = True, checkTiming=False, color = (0, 0, 0), units = 'pix') # checkTiming is due to PsychoPy's latest release where measuring screen rate is shown to participants, in my case it gets stuck, so adding this parameter to prevent that
+# more information on this issue here:
+# https://discourse.psychopy.org/t/is-there-a-way-to-skip-frame-rate-measurement-on-each-initialisation/36232
+# https://github.com/psychopy/psychopy/issues/5937
 
 # Participant data
 
@@ -50,10 +69,17 @@ while ppt_number_taken:
     ppt_number = str(info['Participant number'])
     edf_name = str(info['EDF_name']) # to obtain EDF file name, you could alternatively use ppt_number
     
-    behavioural_file = '/subject_' + ppt_number + '.txt'
+    behavioural_file = '/behavioural/subject_' + ppt_number + '.txt'
     edf_file = edf_name + '.EDF' # remember to add the .edf extension
     if not os.path.isfile(behavioural_file):
         ppt_number_taken = False
+
+# Set up the folder to save .edf files in the STIM PC
+
+results_folder = 'et_results'
+if not os.path.exists(results_folder):
+    os.makedirs(results_folder)
+local_edf = os.path.join(results_folder, edf_file) # we call this at the end to transfer .EDF from ET PC to STIM PC
 
 # To communicate with participants
 
@@ -71,34 +97,27 @@ def message(message_text = "", response_key = "space", duration = 0, height = No
     else:
         time.sleep(duration) # for the feedback
 
-# Screen size
-
-
-win = visual.Window(fullscr = True, checkTiming=False, color = (0, 0, 0), units = 'pix') # checkTiming is due to PsychoPy's latest release where measuring screen rate is shown to participants, in my case it gets stuck, so adding this parameter to prevent that
-# more information on this issue here:
-# https://discourse.psychopy.org/t/is-there-a-way-to-skip-frame-rate-measurement-on-each-initialisation/36232
-# https://github.com/psychopy/psychopy/issues/5937
-
-win_width = win.size[0]
-win_height = win.size[1]
-
 # Load stimuli
+# check section 7.3 to follow this bit of code
+# https://docs.google.com/document/d/1bKIGYGZBmMxaqWgj31S55b8RhHIy8kqqMStaU7Ku7Vg/edit
 
-faces = ['S1.jpg', 'S2.jpg', 'S3.jpg', 'S4.jpg', 'S5.jpg', 'S6.jpg', 'S7.jpg', 'S8.jpg', 'S9.jpg', 'S10.jpg', 'S11.jpg',
-    'H1.jpg', 'H2.jpg', 'H3.jpg', 'H4.jpg', 'H5.jpg', 'H6.jpg', 'H7.jpg', 'H8.jpg', 'H9.jpg', 'H10.jpg', 'H11.jpg']
+ThisExp = data.ExperimentHandler(dataFileName = behavioural_file, extraInfo = info)
+TrialList = data.importConditions('basicscript_conditions.xlsx') 
+trials = data.TrialHandler(TrialList, nReps = 0.5, method = 'random') # we only want 10 trials, not 20
+ThisExp.addLoop(trials)
+
+# Define response keys
 
 response_keys = ['s', 'h']
 
-# Open connection to the Host PC
-# Here we create dummy_mode
-
-dummy_mode = False # assuming we are piloting in our machine, set to False when using the tracker
+# Start of the experiment
+# 1. Open the connection to the ET PC
 
 if dummy_mode:
     et_tracker = pylink.EyeLink(None)
     et_version = 0  # set version to 0, in case running in Dummy mode
 else:
-    try:
+    try: # at the end of the eye-tracking section (before we move onto ioHub, there is a brief discussion on this try-except statements)
         et_tracker = pylink.EyeLink("100.1.1.1")
     except RuntimeError as error:
         dlg = gui.Dlg("Dummy Mode?")
@@ -114,27 +133,18 @@ else:
             print('user cancelled')
             core.quit()
             sys.exit()
-            
-if not dummy_mode:
-    vstr = et_tracker.getTrackerVersionString()
-    et_version = int(vstr.split()[-1].split('.')[0])
-    # print out some version info in the shell
-    print('Running experiment on %s, version %d' % (vstr, et_version))
 
-# To save .edf files
-
-results_folder = 'et_results'
-if not os.path.exists(results_folder):
-    os.makedirs(results_folder)
-local_edf = os.path.join(results_folder, edf_file) # we call this at the end to transfer .EDF from Host to Presentation PC
-
-# Open .EDF file
+# 2. Open the .EDF file
 
 if not dummy_mode:
     et_tracker.openDataFile(edf_file)
 
-# Configure the tracker
-# Put the tracker in offline mode before changing the parameters
+# Add preamble (optional)
+
+preamble_text = 'Basic Script in PsychoPy - Facial emotion detection' 
+et_tracker.sendCommand("add_file_preamble_text '%s'" % preamble_text)
+
+# 3. Configure the tracker
 
 if not dummy_mode:
     et_tracker.setOfflineMode()
@@ -146,6 +156,13 @@ if not dummy_mode:
     et_tracker.sendCommand("calibration_type = HV5")
     et_tracker.sendCommand("screen_pixel_coords = 0 0 %d %d" % (1920-1, 1080-1)) # this needs to be modified to the Display PC screen size you are using
     et_tracker.sendMessage("DISPLAY_COORDS 0 0 %d %d" % (1920-1, 1080-1)) # this needs to be modified to the Display PC screen size you are using
+    
+    # get the tracker version to see what data can be stored
+    
+    vstr = et_tracker.getTrackerVersionString()
+    et_version = int(vstr.split()[-1].split('.')[0])
+    
+    # what events to store
     
     # events to store
     
@@ -165,9 +182,9 @@ if not dummy_mode:
     et_tracker.sendCommand("link_event_filter = %s" % link_event_flags)
     et_tracker.sendCommand("link_sample_data = %s" % link_sample_flags)
     
-    # Experiment starts
-    # We first perform the calibration and validation
-    # In this case, we are doing it without modifying the original set up (i.e., without customizing it)
+# 4. Calibration and validation
+
+# In this example, we are not customising the calibration and validation
 
 message("The calibration will now start. If you double press 'Enter', you can see the camera, or 'c' to start calibrating afterwards.")
 
@@ -176,50 +193,55 @@ if not dummy_mode:
     pylink.openGraphicsEx(genv)
     et_tracker.doTrackerSetup()
 
-# 10 trials, iterate through the loop of faces
+# Communicate that the experiment is about to start
+
 message("The experiment will start now. Press the spacebar to continue.")
+
+# The experiment
 
 trial_index = 0
 
-for i in range(10):
+for trial in trials:
     trial_index += 1
-    # load and create trial stimuli on the fly
-    numpy.random.shuffle(faces)
-    face_st = faces[i]
-    if face_st.startswith('S'):
-        emotion = 'sad'
-    else:
-        emotion = 'happy'
-    
-    image_stimuli = visual.ImageStim(win, image = face_st, pos = (0, 0), units = 'pix') # NB pos coordinates are in pixels: Required by the Graphics Environment, for all units, (0,0) is the center
+
+    # Load the images to display
+
+    image_stimuli = visual.ImageStim(win, image = trial["image"], pos = (0, 0), units = 'pix') # NB pos coordinates are in pixels: Required by the Graphics Environment, for all units, (0,0) is the center
     image_stimuli.size/=6 # rescale
     text_position_y = 0 - int(image_stimuli.size[1]/2) - int(50) 
-    print(text_position_y)
     text_stimuli = visual.TextStim(win, text = "Press 's' if the face is sad, 'h' if the face is happy",
     pos = (0, text_position_y), color = "black", units = 'pix') 
+
+    # Mark the beginning of the trial
+    # Send message to the .EDF file (for later data segmentation) and to the ET PC for us
     
-    # mark trial onset for the eye-tracker
+    et_tracker.sendMessage('TRIALID %d' % trial_index)
+    
+    # record_status_message : show some info on the ET PC
+    # here we show how many trial has been tested
+    # you could put condition, stimuli, etc. whatever is informative for you
+    status_msg = 'TRIAL number %d' % trial_index
+    et_tracker.sendCommand("record_status_message '%s'" % status_msg)
+    
+    # Perform drift correction (drift check)
     
     if not dummy_mode:
-        et_tracker.sendMessage('TRIALID %d' % trial_index) # TRIALID is the 'trigger' that DataViewer uses to segment trials
-        
-        # information to be shown in the host PC
-        # in this case, we want to know what trial we are recording
-        
-        et_tracker.sendCommand("record_status_message '%s'" % trial_index)
+        et_tracker.doDriftCorrect(int(1920/2), int(1080/2), 1, 1)
+
+    # Start recording
     
-    
-    # every trial starts with a drift correction
-    
+    # put tracker in idle/offline mode before recording
+    et_tracker.setOfflineMode()
+
+    # Start recording
+    # arguments: sample_to_file, events_to_file, sample_over_link,
+    # event_over_link (1-yes, 0-no)
     if not dummy_mode:
-        et_tracker.doDriftCorrect(int(win_width/2), int(win_height/2), 1, 1)
-    
-    # start recording
-    if not dummy_mode:
-        et_tracker.setOfflineMode()
         et_tracker.startRecording(1, 1, 1, 1)
     
-    # presentation of visual stimuli
+    # Draw stimuli and wait for participants response
+    # Mark in the .EDF file when images were shown (i.e., send a trigger)
+
     text_stimuli.draw()
     image_stimuli.draw()
     win.flip()
@@ -228,34 +250,50 @@ for i in range(10):
     if not dummy_mode:
         et_tracker.sendMessage('image_onset')
     
+    # wait for a response
+
     resp = event.waitKeys(keyList = response_keys)
+
+    # stop recording, save information about the trial & mark trial end in the .EDF file
     
+    # clear the screen
+    # send a message to clear the Data Viewer screen as well
+    et_tracker.sendMessage('!V CLEAR 128 128 128')
+
+    # stop recording; add 100 msec to catch final events before stopping
+    pylink.pumpDelay(100)
+    et_tracker.stopRecording()
+
     # log information about this trial in the EDF file
     # in this case, what specific stimuli was shown
     # and the emotion shown
+    et_tracker.sendMessage('!V TRIAL_VAR image %s' % trial["image"])
+    et_tracker.sendMessage('!V TRIAL_VAR emotion %s' % trial["emotion"])
+
+    # send a 'TRIAL_RESULT' message to mark the end of trial, see Data
+    # Viewer User Manual, "Protocol for EyeLink Data to Viewer Integration"
+    et_tracker.sendMessage('TRIAL_RESULT %d' % pylink.TRIAL_OK)
+
+    # Next trial
+
+    ThisExp.nextEntry()
     
-    if not dummy_mode:
-        et_tracker.sendMessage('!V TRIAL_VAR image %s' % face_st)
-        et_tracker.sendMessage('!V TRIAL_VAR emotion %s' % emotion)
-    
-    # stop recording
-    if not dummy_mode:
-        pylink.pumpDelay(100)
-        et_tracker.stopRecording()
-        et_tracker.sendMessage("TRIAL_RESULT %d" % pylink.TRIAL_OK) # used by DataViewer to segment the data
-    
-    trial_index += 1
-    
-# End of trials
-# save EDF file and close connection with the Host PC
+# End of experiment
+
+# We need to close the data file, transfer it from ET PC to STIM PC and then close the connection between both PCs (plus exist PsychoPy)
 
 if not dummy_mode:
     et_tracker.setOfflineMode()
     pylink.pumpDelay(100)
-    et_tracker.closeDataFile()
+    et_tracker.closeDataFile() # close the file
     pylink.pumpDelay(500)
-    et_tracker.receiveDataFile(edf_file, local_edf)
-    et_tracker.close()
+    et_tracker.receiveDataFile(edf_file, local_edf) # transfer the file
+    et_tracker.close() # close the link
+
+# Close & quit PsychoPy
 
 message("That's the end of the experiment. Press the spacebar to exit.")
+
 win.close()
+core.quit()
+sys.exit()
